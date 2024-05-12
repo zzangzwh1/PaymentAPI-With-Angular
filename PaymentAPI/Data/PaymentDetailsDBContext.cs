98bee957d60a65842255689c527c0e6956ec93bd
Wonhyuk Cho <wcho2@nait.ca>
2024-05-11 19:30:29 -0600

    Add project files.

---
+++ PaymentAPI/Data/PaymentDetailsDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentAPI.Models;
+
+
+
+namespace PaymentAPI.Data
+{
+    public class PaymentDetailsDBContext : DbContext
+    {
+        public PaymentDetailsDBContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+
+    }
+}
