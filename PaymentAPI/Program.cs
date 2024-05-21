d993e13488050bcbadef3f7d0044b9253f0f1655
Wonhyuk Cho <wcho2@nait.ca>
2024-05-20 19:55:33 -0600

    Server-PaymentAPI

--- PaymentAPI/Program.cs
+++ PaymentAPI/Program.cs
@@ -21,6 +21,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors(option => option.WithOrigins("http://localhost:4200").AllowAnyMethod());
 
 app.UseHttpsRedirection();
 
